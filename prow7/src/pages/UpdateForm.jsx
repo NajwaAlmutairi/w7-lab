f78d28f2a7a431b31e9dcf50f4503e94e6082205
najwa <NajwaBuayjan@gmail.com>
2024-10-06 14:06:17 +0300

    solving lab 2

--- prow7/src/pages/UpdateForm.jsx
+++ prow7/src/pages/UpdateForm.jsx
@@ -30,41 +30,6 @@ function UpdateForm() {
 
     return (
         <>
-            {/* <div className="flex justify-center items-center min-h-screen bg-gray-800">
-      <div className='flex justify-between bg-zinc-500 w-[70vw] max-sm:flex-col '>
-          <div className='flex flex-col gap-3 items-center justify-center'>
-            <img src="https://finalspaceapi.com/api/character/avatar/quinn_ergon.jpg"
-              alt="image"
-              className='w-40 h-40' />
-            <Link to={'/'}>
-              <button className='btn btn-circle bg-blue-400 min-h-7 h-9 w-20 hover:bg-blue-500 text-white border-none'>Home</button>
-            </Link>
-          </div> */}
-
-            {/* <div className="flex justify-center items-center min-h-screen bg-gray-800">
-            <div className='flex-col gap-2 bg-white p-6 rounded-lg shadow-md w-full max-w-md '>
-                <div className="mb-4">
-                    <label className="block text-gray-700 mb-2" htmlFor="url">URL</label>
-                    <input
-                        type="url"
-                        id="url"
-                        value={url}
-                        onChange={(e) => setUrl(e.target.value)}
-                        className="w-full p-2 border border-gray-300 rounded"
-                        required
-                    />
-                </div>
-                <button className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">
-                    update
-                </button>
-
-                <Link to={'/'}>
-                    <button className='btn btn-circle bg-blue-400 min-h-7 h-9 w-20 hover:bg-blue-500 text-white border-none'>Home</button>
-                </Link>
-                </div>
-    </div> */}
-
-
             <div className="flex justify-center items-center min-h-screen bg-gray-800">
                 <div className='flex flex-col gap-4 bg-white p-6 rounded-lg shadow-md w-full max-w-md'>
                     <h2 className="text-xl font-semibold mb-4">Update URL</h2>
